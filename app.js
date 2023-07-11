2e80f735019502420bd92cc073171c4de3e33e55
jlawthom <jacklawthom@yahoo.co.uk>
2023-07-11 15:29:06 +0100

    Enabled https

--- app.js
+++ app.js
@@ -1,30 +1,47 @@
 const express = require('express');
 const axios = require('axios');
 
+var fs = require('fs');
+var https = require('https');
+var privateKey  = fs.readFileSync('sslcert/key.pem', 'utf8');
+var certificate = fs.readFileSync('sslcert/cert.pem', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate};
 const app = express();
 
-app.listen(3000, () => console.log('Listening on port 3000'));
+var httpsServer = https.createServer(credentials, app);
 
-app.get('/', (req, res) => {
-    const text = 'API Running!! You better go catch it';
-    res.json(text);
+httpsServer.listen(3000, () => console.log('Listening on port 3000'));
 
-    axios.post('https://envjnywsdj5nf.x.pipedream.net/', {
-        firstName: 'Fred',
-        lastName: 'Flintstone'
-    },
-    {
-        headers: {
-            myHeader1: "lovely cushioned header, for GERRARRRD ..."
-        }
-    })
-    .then((response) => {
-        console.log('response:');
-        console.log(response);
-    })
-    .catch((err) => {
-        console.log('error:');
-        console.log(err);
-    })
+const botId = "1234-abc-123-abcd";
+const environment = "draft";
+const convId = "1234-abc-123-abcd";
 
-})
+app.get(`/v1/bots/${botId}/environments`, (req, res) => {
+    const response = ["draft", "alpha"];
+
+    res.statusMessage = "Bot Found";
+    res.status(200);
+    res.json(response);
+});
+
+app.get(`/v1/bots/${botId}/environments/${environment}/state`, (req, res) => {
+    const response = {
+        "state": "online",
+        "version": "0.0.1"
+    };
+
+    res.statusMessage = "Bot Found";
+    res.status(200);
+    res.json(response);
+});
+
+app.put(`/v1/bots/${botId}/environments/${environment}/conversations/${convId}`, (req, res) => {
+    const response = {
+
+    };
+
+    res.statusMessage = "Created";
+    res.status(200);
+    res.json(response);
+});
