6142cc7587b67cbc1f36dd827de216efa5d749f4
jlawthom <jacklawthom@yahoo.co.uk>
2023-07-12 14:47:18 +0100

    Added logs

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const axios = require('axios');
 
 var fs = require('fs');
 var https = require('https');
-var privateKey  = fs.readFileSync('sslcert/key.pem', 'utf8');
-var certificate = fs.readFileSync('sslcert/cert.pem', 'utf8');
+var privateKey = fs.readFileSync('sslcert/key.pem', 'utf8');
+var certificate = fs.readFileSync('sslcert/fullchain.pem', 'utf8');
 
-var credentials = {key: privateKey, cert: certificate};
+var credentials = { key: privateKey, cert: certificate };
 const app = express();
+
+app.use(express.json())
 
 var httpsServer = https.createServer(credentials, app);
 
@@ -18,6 +20,10 @@ const environment = "draft";
 const convId = "1234-abc-123-abcd";
 
 app.get(`/v1/bots/${botId}/environments`, (req, res) => {
+    console.log("");
+    console.log("Req:");
+    console.log(req.body);
+
     const response = ["draft", "alpha"];
 
     res.statusMessage = "Bot Found";
@@ -26,6 +32,10 @@ app.get(`/v1/bots/${botId}/environments`, (req, res) => {
 });
 
 app.get(`/v1/bots/${botId}/environments/${environment}/state`, (req, res) => {
+    console.log("");
+    console.log("Req:");
+    console.log(req.body);
+
     const response = {
         "state": "online",
         "version": "0.0.1"
@@ -37,8 +47,56 @@ app.get(`/v1/bots/${botId}/environments/${environment}/state`, (req, res) => {
 });
 
 app.put(`/v1/bots/${botId}/environments/${environment}/conversations/${convId}`, (req, res) => {
+    console.log("");
+    console.log("Req:");
+    console.log(req.body);
+
     const response = {
 
+    };
+
+    res.statusMessage = "Created";
+    res.status(200);
+    res.json(response);
+});
+
+app.post(`/v1/bots/${botId}/environments/${environment}/conversations/${convId}/events`, (req, res) => {
+    console.log("");
+    console.log("Req:");
+    console.log(req.body);
+
+    const response = {
+        "response": [
+            {
+                "type": "STRUCTURED_CONTENT",
+                "data": {
+                    "metadata": [{ "type": "ExternalId", "id": "ABCD1234" }],
+                    "structuredContent": {
+                        "type": "vertical",
+                        "elements": [
+                            {
+                                "type": "button",
+                                "click": {
+                                    "actions": [
+                                        { "text": "Recommend me a movie", "type": "publishText" }
+                                    ]
+                                },
+                                "title": "Recommend a movie"
+                            }
+                        ]
+                    }
+                }
+            }
+        ],
+        "analytics": {
+            "intents": [
+                {
+                    "id": "base-structured-content",
+                    "description": "Structured Content Message",
+                    "confidenceScore": 1
+                }
+            ]
+        }
     };
 
     res.statusMessage = "Created";
